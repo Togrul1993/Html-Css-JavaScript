2bced39c6a3a0a67a3a0d64fb690ce2bf8473f57
Togrul Salmanli <togrulsalmanli33@gmail.com>
2020-08-26 22:01:03 +0400

    Form js

---
+++ form.js
@@ -0,0 +1,42 @@
+'use strict';
+const signupModal=document.querySelector(".signup-form-wrapper");
+
+const loginModal=document.querySelector(".login-form-wrapper");
+
+const signupBtn=document.querySelector(".signup-btn");
+
+const loginBtn=document.querySelector(".login-btn");
+
+const signupX=document.querySelector('.singup-x');
+
+const loginX=document.querySelector('.login-x');
+
+const formContainer=document.querySelector('.form-container');
+
+signupBtn.addEventListener('click',()=>{
+	signupModal.classList.add('display');
+	formContainer.classList.add('disable');
+});
+
+loginBtn.addEventListener('click',()=>{
+	loginModal.classList.add('display');
+	formContainer.classList.add('disable');
+});
+
+
+signupX.addEventListener('click',()=>{
+	signupModal.classList.remove('display');
+	formContainer.classList.remove('disable');
+});
+
+loginX.addEventListener('click',()=>{
+	loginModal.classList.remove('display');
+	formContainer.classList.remove('disable');
+});
+
+
+
+
+
+
+
